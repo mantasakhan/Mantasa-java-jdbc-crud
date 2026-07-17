d137eff338e66eb08e43c6ae4e19f86c0a72102f
mantasakhan <kmantasa78@gmail.com>
2026-07-17 17:43:33 +0530

    Update SearchProduct.java

--- src/my_crud/SearchProduct.java
+++ src/my_crud/SearchProduct.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class SearchProduct1 {
+public class SearchProduct {
 
 	public static void main(String[] args) {
 		
